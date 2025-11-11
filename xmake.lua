b1e1fef688ee47c69909ef4516631af760430677
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2025-11-12 00:23:13 +0800

    xmake

--- xmake.lua
+++ xmake.lua
@@ -7,14 +7,13 @@ if is_mode("debug") then
 	set_policy("build.sanitizer.address", true)
 	set_policy("build.sanitizer.leak", true)
 	set_policy("build.sanitizer.undefined", true)
-elseif is_mode("tsan") then
+elseif is_mode("debug_tsan") then
 	set_policy("build.sanitizer.thread", true)
 end
 
 add_requires("imgui", { configs = { glfw_opengl3 = true } })
 add_requires("glfw")
 add_requires("opengl")
-
 
 target("engine")
     set_kind("static")
