27e81e9fda1cb8e18a2b7d598166b15ff486a90f
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2025-11-11 19:14:15 +0800

    re

--- src/engine/state.h
+++ src/engine/state.h
@@ -7,11 +7,6 @@
 namespace engine {
 
 struct State {
-    std::string title;
-    int width = 1280;
-    int height = 720;
-    bool vsync = true;
-    float scale = 1;
     std::shared_ptr<glfw::Window> window;
     bool stopSignal = false;
 };
