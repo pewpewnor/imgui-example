dc24fc0f551ca4a294d29181adcae94ad897d4df
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2025-11-11 19:23:45 +0800

    pro

--- src/engine/step.h
+++ src/engine/step.h
@@ -9,15 +9,15 @@ template <typename TState>
     requires std::derived_from<TState, engine::State>
 class Step {
 public:
-    Step(const std::shared_ptr<TState>& state) : state(state) {}
-
     Step(const Step&) = default;
-    Step(Step&&) = delete;
+    Step(Step&&) = default;
     Step& operator=(const Step&) = default;
-    Step& operator=(Step&&) = delete;
+    Step& operator=(Step&&) = default;
     virtual ~Step() = default;
 
 protected:
+    Step(const std::shared_ptr<TState>& state) : state(state) {}
+
     std::shared_ptr<TState> state;
 };
 
