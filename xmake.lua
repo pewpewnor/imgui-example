7edf25c6fae0a35cd440b45a5e7b46b59a3212b4
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2025-11-13 03:53:36 +0800

    add my own

--- xmake.lua
+++ xmake.lua
@@ -10,24 +10,109 @@ end
 set_languages("c++23")
 set_warnings("allextra", "pedantic", "error")
 
+package("imgui-sfml")
+    set_homepage("https://github.com/eliasdaler/imgui-sfml")
+    set_description("Dear ImGui binding for use with SFML")
+    set_license("MIT")
+
+    add_urls("https://github.com/eliasdaler/imgui-sfml/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/eliasdaler/imgui-sfml.git")
+
+    add_versions("v3.0", "561a04407dcc913b8f1c25a164ad4907852b51fb4f11907d1ab9db6351af911b")
+
+    add_deps("cmake")
+    add_deps("opengl", {optional = true})
+
+    if is_plat("windows", "mingw") then
+        add_syslinks("imm32")
+    end
+
+    add_links("ImGui-SFML")
+
+    on_load(function(package)
+        if package:version():eq("3.0") then
+            package:add("deps", "imgui <=1.91.8")
+        else
+            package:add("deps", "imgui")
+        end
+
+        if package:is_plat("linux") and package:config("shared") then
+            package:add("deps", "sfml", {configs = {shared = true}})
+        else
+            package:add("deps", "sfml")
+        end
+        if package:is_plat("windows", "mingw") and package:config("shared") then
+            package:add("defines", "IMGUI_SFML_SHARED_LIB=1")
+        end
+    end)
+
+    on_install("macosx", "linux", "windows", "mingw", function (package)
+        local imgui_version = ""
+        if package:version():eq("3.0") then
+            imgui_version = " <=1.91.8"
+        end
+
+        io.writefile("xmake.lua", string.format([[
+            add_rules("mode.release", "mode.debug")
+            add_requires("imgui%s")
+            if is_plat("linux") and is_kind("shared") then
+                add_requires("sfml", {configs = {shared = true}})
+            else
+                add_requires("sfml")
+            end
+            add_requires("opengl", {optional = true})
+            target("ImGui-SFML")
+                set_kind("$(kind)")
+                add_files("imgui-SFML.cpp")
+                add_headerfiles("*.h")
+                add_includedirs(".")
+                add_packages("imgui", "sfml", "opengl")
+                set_languages("c++17")
+                add_defines("IMGUI_USER_CONFIG=\"imconfig-SFML.h\"")
+                if is_plat("windows", "mingw") then
+                    add_syslinks("imm32")
+                    if is_kind("shared") then
+                        add_defines("IMGUI_SFML_SHARED_LIB=1", "IMGUI_SFML_EXPORTS")
+                    end
+                end
+                add_rules("utils.install.pkgconfig_importfiles")
+                add_rules("utils.install.cmake_importfiles")
+        ]], imgui_version))
+        local configs = {}
+        if package:config("shared") then
+            configs.kind = "shared"
+        end
+        import("package.tools.xmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            #include "imgui.h"
+            #include "imgui-SFML.h"
+            #include <SFML/Graphics/CircleShape.hpp>
+            #include <SFML/Graphics/RenderWindow.hpp>
+            #include <SFML/System/Clock.hpp>
+            #include <SFML/Window/Event.hpp>
+            void test() {
+                sf::RenderWindow window(sf::VideoMode({640, 480}), "ImGui + SFML = <3");
+                window.setFramerateLimit(60);
+                ImGui::SFML::Init(window);
+            }
+        ]]}, {configs = {languages = "c++17"}}))
+    end)
+package_end()
+
 add_requires("imgui 1.91.8", { configs = { backend = "none" } })
 add_requires("sfml 3.0.1")
-
-target("imgui-sfml")
-    set_kind("static")
-    add_files("vendor/imgui-sfml/*.cpp")
-    add_includedirs("vendor/imgui-sfml", { public = true })
-    add_packages("imgui", "sfml")
-    add_syslinks("GL")
+add_requires("imgui-sfml 3.0.0")
 
 target("engine")
     set_kind("static")
-    add_packages("imgui", "sfml")
-    add_deps("imgui-sfml")
+    add_packages("imgui", "sfml", "imgui-sfml")
 
 target("app")
     set_kind("binary")
     add_files("src/app/**.cpp")
     add_includedirs("src")
     add_deps("engine")
-    add_packages("imgui", "sfml")
+    add_packages("imgui", "sfml", "imgui-sfml")
