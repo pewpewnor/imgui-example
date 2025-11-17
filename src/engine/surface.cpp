1b8f0f2e8c13960c4c21ac9f95fe2c2aa9ab86fe
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2025-11-18 02:58:25 +0800

    sync

--- src/engine/surface.cpp
+++ src/engine/surface.cpp
@@ -4,23 +4,18 @@
 
 engine::Surface::Surface(
     const std::shared_ptr<engine::EngineState>& engineState,
-    const std::string& title, int width, int height, bool vsync)
+    const std::string& title, int width, int height)
     : engineState_(engineState),
       title_(title),
       width_(width),
-      height_(height),
-      vsync_(vsync) {}
+      height_(height) {}
 
 void engine::Surface::onStartup() {
     engineState_->window =
         sf::RenderWindow(sf::VideoMode({static_cast<unsigned int>(width_),
                                         static_cast<unsigned int>(height_)}),
                          title_);
-    if (vsync_) {
-        engineState_->window.setVerticalSyncEnabled(true);
-    } else {
-        engineState_->window.setFramerateLimit(60);
-    }
+    engineState_->window.setVerticalSyncEnabled(true);
     if (!ImGui::SFML::Init(engineState_->window)) {
         throw std::runtime_error("failed to initialize ImGui-SFML");
     }
