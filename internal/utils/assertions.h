1bd35fe591102e67a1fe9e5d23eaa298aa43c59f
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2025-11-22 05:38:04 +0800

    assert debug

--- internal/utils/assertions.h
+++ internal/utils/assertions.h
@@ -3,15 +3,18 @@
 #ifndef NDEBUG
 #include <cassert>
 
-#define ASSERT(condition, message) assert((condition) && (message))
-#define ASSERT_SOFT(condition, message) ASSERT(condition, message)
+#define ASSERT_DEBUG(condition, message) assert((condition) && (message))
+#define ASSERT_HARD(condition, message) ASSERT_DEBUG(condition, message)
+#define ASSERT_SOFT(condition, message) ASSERT_DEBUG(condition, message)
 #else
 #include <spdlog/spdlog.h>
 
 #include <stdexcept>
 #include <string>
 
-#define ASSERT(condition, message)                                 \
+#define ASSERT_DEBUG(condition, message)
+
+#define ASSERT_HARD(condition, message)                            \
     do {                                                           \
         if (!(condition)) {                                        \
             spdlog::error("Hard assertion failed: {}", (message)); \
